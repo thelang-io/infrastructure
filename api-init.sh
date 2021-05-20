b7034210778189afa2368d134c531a28723a28ed
Aaron Delasy <aaron@thelang.io>
2021-05-21 02:24:21 +0300

    Install nodejs on EC2 machine

--- api-init.sh
+++ api-init.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-apt update -y
-apt upgrade -y
-apt install -y build-essential nginx python3-certbot python3-certbot-nginx
-curl -L https://git.io/n-install | bash
-n lts
-apt autoclean
-apt autoremove
-apt clean
+#!/bin/sh -e
+apt-get update -y
+apt-get upgrade -y
+curl -L https://deb.nodesource.com/setup_lts.x | bash
+apt-get install -y build-essential certbot nginx nodejs python3-certbot-nginx
+apt-get autoclean
+apt-get autoremove
+apt-get clean
 rm -rf /tmp/* /var/tmp/*
