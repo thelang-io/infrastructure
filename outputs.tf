92a458e5ffcfc9b2ab273b11a7667985127bfeec
Aaron Delasy <aaron@thelang.io>
2021-05-20 19:38:32 +0300

    Add copyright notice for all terraform files

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
+#
+# Copyright (c) Aaron Delasy
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+
 output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.the-api-ec2.id
--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+#
+# Copyright (c) Aaron Delasy
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+#
+
 variable "eip_allocation_id" {
   description = "Value of the allocation id for Elastic IP Address"
   type        = string
