7fc9327f33f0141df056dd3675b38e6db826f09e
Aaron Delasy <aaron@thelang.io>
2021-05-20 22:07:37 +0300

    Clean tmp directories after installing dependencies

--- api-init.sh
+++ api-init.sh
@@ -4,3 +4,7 @@ apt upgrade -y
 apt install -y build-essential nginx python3-certbot python3-certbot-nginx
 curl -L https://git.io/n-install | bash
 n lts
+apt autoclean
+apt autoremove
+apt clean
+rm -rf /tmp/* /var/tmp/*
