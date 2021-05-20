4583b286088a38113c0799a736c741aaed89181d
Aaron Delasy <aaron@thelang.io>
2021-05-20 18:56:55 +0300

    Add security group configuration

--- main.tf
+++ main.tf
@@ -14,11 +14,36 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_instance" "api_server" {
-  ami           = "ami-05f7491af5eef733a"
-  instance_type = "t2.micro"
+resource "aws_security_group" "the-api-sg" {
+  name = "the-api-sg"
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    name = "the-api"
+    Name = "the-api"
+  }
+}
+
+resource "aws_instance" "the-api-ec2" {
+  ami                    = "ami-05f7491af5eef733a"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.the-api-sg.id]
+
+  tags = {
+    Name = "the-api"
   }
 }
