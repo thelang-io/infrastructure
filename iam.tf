88567b8f16715d5c3f64b3f0502e851839714da5
Aaron Delasy <aaron@thelang.io>
2021-05-28 17:32:10 +0300

    Add code-pipeline IAM role

--- iam.tf
+++ iam.tf
@@ -39,22 +39,25 @@ data "aws_iam_policy_document" "code_build_role_policy" {
     resources = ["*"]
   }
 
-  //  statement {
-  //    actions = ["ec2:CreateNetworkInterfacePermission"]
-  //    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
-  //
-  //    condition {
-  //      test     = "StringEquals"
-  //      variable = "ec2:Subnet"
-  //      values = [aws_subnet.example1.arn, aws_subnet.example2.arn]
-  //    }
-  //
-  //    condition {
-  //      test     = "StringEquals"
-  //      variable = "ec2:AuthorizedService"
-  //      values = [aws_s3_bucket.api.arn, "${aws_s3_bucket.code_pipeline.arn}/*"]
-  //    }
-  //  }
+//  statement {
+//    actions = ["ec2:CreateNetworkInterfacePermission"]
+//    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
+//
+//    condition {
+//      test     = "StringEquals"
+//      variable = "ec2:Subnet"
+//      values = [aws_subnet.example1.arn, aws_subnet.example2.arn]
+//    }
+//
+//    condition {
+//      test     = "StringEquals"
+//      variable = "ec2:AuthorizedService"
+//      values = [
+//        aws_s3_bucket.code_pipeline.arn,
+//        "${aws_s3_bucket.code_pipeline.arn}/*"
+//      ]
+//    }
+//  }
 
   statement {
     actions   = ["s3:*"]
@@ -63,6 +66,17 @@ data "aws_iam_policy_document" "code_build_role_policy" {
       aws_s3_bucket.code_pipeline.arn,
       "${aws_s3_bucket.code_pipeline.arn}/*"
     ]
+  }
+}
+
+data "aws_iam_policy_document" "code_pipeline_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
   }
 }
 
@@ -75,4 +89,9 @@ resource "aws_iam_role_policy" "code_build" {
   name   = "AWSCodeBuildServiceRolePolicy"
   role   = aws_iam_role.code_build.id
   policy = data.aws_iam_policy_document.code_build_role_policy.json
+}
+
+resource "aws_iam_role" "code_pipeline" {
+  name               = "AWSCodePipelineServiceRole"
+  assume_role_policy = data.aws_iam_policy_document.code_pipeline_role.json
 }
