6cfccd34b93b4d61a77c2181c058111a7e6c7211
Aaron Delasy <aaron@thelang.io>
2021-05-28 16:06:30 +0300

    Move backend configuration back to main

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@
 #
 
 terraform {
+  backend "s3" {
+    bucket  = "private.thelang.io"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
