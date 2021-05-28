8b74e420383ec42b3a490fbc8bf1855d1029744c
Aaron Delasy <aaron@thelang.io>
2021-05-28 17:29:16 +0300

    Add code-build IAM role policy

--- iam.tf
+++ iam.tf
@@ -14,7 +14,65 @@ data "aws_iam_policy_document" "code_build_role" {
   }
 }
 
+data "aws_iam_policy_document" "code_build_role_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs"
+    ]
+
+    resources = ["*"]
+  }
+
+  //  statement {
+  //    actions = ["ec2:CreateNetworkInterfacePermission"]
+  //    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
+  //
+  //    condition {
+  //      test     = "StringEquals"
+  //      variable = "ec2:Subnet"
+  //      values = [aws_subnet.example1.arn, aws_subnet.example2.arn]
+  //    }
+  //
+  //    condition {
+  //      test     = "StringEquals"
+  //      variable = "ec2:AuthorizedService"
+  //      values = [aws_s3_bucket.api.arn, "${aws_s3_bucket.code_pipeline.arn}/*"]
+  //    }
+  //  }
+
+  statement {
+    actions   = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.code_pipeline.arn,
+      "${aws_s3_bucket.code_pipeline.arn}/*"
+    ]
+  }
+}
+
 resource "aws_iam_role" "code_build" {
-  name               = "aws-codebuild-the-api-service-role"
+  name               = "AWSCodeBuildServiceRole"
   assume_role_policy = data.aws_iam_policy_document.code_build_role.json
+}
+
+resource "aws_iam_role_policy" "code_build" {
+  name   = "AWSCodeBuildServiceRolePolicy"
+  role   = aws_iam_role.code_build.id
+  policy = data.aws_iam_policy_document.code_build_role_policy.json
 }
