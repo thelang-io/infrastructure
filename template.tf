6b1947485d55b564d935b44ae29156ce31cffffd
Aaron Delasy <aaron@thelang.io>
2021-05-29 20:42:33 +0300

    Add template_file configuration for api_user_data

---
+++ template.tf
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2021-present Aaron Delasy
+# Licensed under the MIT License
+#
+
+data "template_file" "api_build_buildspec" {
+  template = file("templates/te2135.yml")
+}
+
+data "template_file" "api_test_buildspec" {
+  template = file("templates/te2134.yml")
+}
+
+data "template_file" "api_user_data" {
+  template = file("templates/api-user-data.yml")
+}
