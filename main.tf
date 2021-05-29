725254daf70c3383080edf299c55b60515997c28
Aaron Delasy <aaron@thelang.io>
2021-05-29 15:44:45 +0300

    Add description of actions before using infrastructure

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@
 # Licensed under the MIT License
 #
 
+#
+# Before using:
+#  1. Create Access Key
+#  2. Create S3 bucket "the-private"
+#  3. Create DynamoDB table "the-private"
+#  3. Create CodeStar connection
+#
+
 terraform {
   backend "s3" {
     bucket  = "the-private"
