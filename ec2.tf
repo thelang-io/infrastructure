ea9ab31f4206ef16600e906b4827d1c9ecfb01e7
Aaron Delasy <aaron@thelang.io>
2021-05-30 18:15:39 +0300

    Add instance profile for API EC2 instance

--- ec2.tf
+++ ec2.tf
@@ -34,6 +34,7 @@ resource "aws_security_group" "public" {
 resource "aws_instance" "api" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.code_deploy_ec2.name
   key_name               = "MacBook Pro (13-inch, 2018)"
   user_data              = data.template_file.api_user_data.rendered
   vpc_security_group_ids = [aws_security_group.public.id]
