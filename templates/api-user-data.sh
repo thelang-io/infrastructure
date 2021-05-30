b1be58bcb72227a955a3291c5aff92a3087f5bb2
Aaron Delasy <aaron@thelang.io>
2021-05-30 19:58:03 +0300

    Install CodeDeploy agent in EC2 user_data

--- templates/api-user-data.sh
+++ templates/api-user-data.sh
@@ -7,10 +7,18 @@
 
 apt-get update -y
 apt-get upgrade -y
+
 curl -L https://deb.nodesource.com/setup_lts.x | bash
-apt-get install -y nodejs
+apt-get install -y nodejs ruby-full
+
+curl -L https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install -o install
+chmod +x ./install
+./install auto > /dev/null
+
 npm install -g pm2
 pm2 startup -u ubuntu --hp /home/ubuntu
+
 mkdir -p /app
 printf "%s" "${ecosystem_config_content}" > /ecosystem.config.js
+
 reboot
