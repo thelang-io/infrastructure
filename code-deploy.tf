2daa25d4af938c9dd794ab5fb26502cdacfb2b70
Aaron Delasy <aaron@thelang.io>
2021-05-30 18:16:07 +0300

    Create CodeDeploy configuration

---
+++ code-deploy.tf
@@ -0,0 +1,24 @@
+#
+# Copyright (c) 2021-present Aaron Delasy
+# Licensed under the MIT License
+#
+
+resource "aws_codedeploy_app" "api" {
+  compute_platform = "Server"
+  name             = "the-api"
+}
+
+resource "aws_codedeploy_deployment_group" "api" {
+  app_name              = aws_codedeploy_app.api.name
+  deployment_group_name = "the-api"
+  service_role_arn      = aws_iam_role.code_deploy.arn
+  depends_on            = [aws_iam_role_policy_attachment.aws_code_deploy]
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      type  = "KEY_AND_VALUE"
+      value = "the-api"
+    }
+  }
+}
