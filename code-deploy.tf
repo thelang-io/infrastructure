60603e9eb49927f4f4d2da1c899c5af43a2bcd8c
Aaron Delasy <aaron@thelang.io>
2021-05-31 23:55:18 +0300

    Change names for CodeDeploy resources

--- code-deploy.tf
+++ code-deploy.tf
@@ -5,12 +5,12 @@
 
 resource "aws_codedeploy_app" "api" {
   compute_platform = "Server"
-  name             = "the-api"
+  name             = "the-api-codedeploy"
 }
 
 resource "aws_codedeploy_deployment_group" "api" {
   app_name              = aws_codedeploy_app.api.name
-  deployment_group_name = "the-api"
+  deployment_group_name = "the-api-codedeploy-group"
   service_role_arn      = aws_iam_role.code_deploy.arn
   depends_on            = [aws_iam_role_policy.code_deploy]
 
