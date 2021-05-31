db4989a368ae1c0127003e0b9fe1acc44978ef75
Aaron Delasy <aaron@thelang.io>
2021-05-31 23:14:56 +0300

    Add auth_token variable

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@
 # Licensed under the MIT License
 #
 
+variable "auth_token" {
+  description = "Value of the authentication token"
+  type        = string
+}
+
 variable "code_star_connection_arn" {
   description = "Value of the arn for CodeStar connection"
   type        = string
