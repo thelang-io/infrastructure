dcaaf70722158114e118e9db052d33b503b6e249
Aaron Delasy <aaron@thelang.io>
2022-09-18 15:36:05 +0300

    Move security groups to separate file

--- ec2.tf
+++ ec2.tf
@@ -3,34 +3,6 @@
 # Licensed under the MIT License
 #
 
-resource "aws_security_group" "public" {
-  name = "public"
-
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
 resource "aws_instance" "api" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
